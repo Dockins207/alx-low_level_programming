14152e41515fa428eb5f007347e4d585e4b68abb
Dockins207 <otienodockins48@gmail.com>
2023-08-31 20:47:08 +0300

    bit manipulation

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * unsigned int binary_to_uint - a function
+ * that converts binary to an unsigned integer.
+ * @b: contant argument to be taken.
+ * Return: unsigned int if success.
+ * or 0 if not.
+ * exit if b is NULL.
+ */
+unsigned int binary_to_uint(const char *b);
+{
+	unsigned int Number = 0;
+	int y = 0;
+
+	if (b == NULL)
+	{
+		return (0);
+	}
+	for (y = 0; y[b]; y++)
+	{
+		if (b[y] < '0' || b[y] > '1')
+			return (0);
+		Number = Number * 2 + (b[y] - '0');
+	}
+
+	return (Number);
+}
---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * void print_binary - a function that,
+ * print the binary representation of a number.
+ *@n: argument 
+ *Return: binary representation of a number.
+ */
+
+void print_binary(unsigned long int n);
+{
+
