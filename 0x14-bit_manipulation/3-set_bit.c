0dec5fbf14bf4bebb5ce7dfb2933c7a4a09dce34
Dockins207 <otienodockins48@gmail.com>
2023-09-22 12:36:27 -0700

    bit manipulatio

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,19 +1,17 @@
 #include "main.h"
 
 /**
- * int set_bit-function that sets the,value of a bit to 1 at a given index.
- * @n: number of sets
- * @index: bit index,
- * Return: 1 if successful,
- * -1 if there is an error.
+ * set_bit - sets a bit at a given index to 1
+ * @n: pointer to the number to change
+ * @index: index of the bit to set to 1
+ *
+ * Return: 1 for success, -1 for failure
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > 32)
-
+	if (index > 63)
 		return (-1);
 
-	*n = ((1ul << index) || *n);
-
+	*n = ((1UL << index) | *n);
 	return (1);
 }
