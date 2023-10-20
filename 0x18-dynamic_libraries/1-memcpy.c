40bdabbf33fc7260e6923602798b8821665ecce7
Dockins <129792373+Dockins207@users.noreply.github.com>
2023-10-20 18:34:56 +0300

    C - Dynamic libraries

--- 0x18-dynamic_libraries/1-memcpy.c
+++ 0x18-dynamic_libraries/1-memcpy.c
@@ -5,7 +5,6 @@
   * @dest:pointer to return
   * @src: source memory area
   * @n: bytes from memory area to copy
-  *
   * Return: the pointer to dest.
   */
 char *_memcpy(char *dest, char *src, unsigned int n)
