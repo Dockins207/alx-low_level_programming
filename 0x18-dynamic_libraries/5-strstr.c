d225c1f3e387e778e12ddfdfa63edd7c344a1520
Dockins <129792373+Dockins207@users.noreply.github.com>
2023-10-20 18:39:51 +0300

    C - Dynamic libraries

--- 0x18-dynamic_libraries/5-strstr.c
+++ 0x18-dynamic_libraries/5-strstr.c
@@ -4,7 +4,6 @@
   * _strstr - locate a substring
   * @haystack: the string to search
   * @needle: the string to find
-  *
   * Return: char value
   */
 char *_strstr(char *haystack, char *needle)
