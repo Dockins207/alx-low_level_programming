ff38a9328ba2b16754ff503c3e7f267113066d00
Dockins207 <otienodockins48@gmail.com>
2023-10-20 14:53:19 -0700

    C - Dynamic libraries

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -D /tmp https://github.com/Dockins207/alx-low_level_programming/raw/master/0x18-dynamic_libraries/can_win.so
-export LD_PRELOAD=/tmp/can_win.so
+gcc -shared -o my_printf.so -fPIC test.c
+export LD_PRELOAD=my_printf.so:$LD_LIBRARY_PATH
