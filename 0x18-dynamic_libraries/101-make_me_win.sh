56ba4781b4b19be71fd148816731d6a7f88fcb1e
Dockins207 <otienodockins48@gmail.com>
2023-10-20 13:57:58 -0700

    C - Dynamic libraries

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -D /temp https://github.com/Dockins207/alx-low_level_programming/raw/main/0x18-dynamic_libraries/make_me_win.so
-export LD_PRELOAD=/temp/make_me_win.so
+wget -D /temp https://github.com/Dockins207/alx-low_level_programming/raw/main/0x18-dynamic_libraries/libgiga.so
+export LD_PRELOAD=/temp/libgiga.so
