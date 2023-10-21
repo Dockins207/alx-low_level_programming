10faaea49f071d92a3c4c527a1e998acbdac3253
Dockins207 <otienodockins48@gmail.com>
2023-10-20 22:30:40 -0700

    C - Dynamic libraries C

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcc -shared -o my_printf.so -fPIC test.c
-export LD_PRELOAD=my_printf.so:$LD_LIBRARY_PATH
+gcc -shared -o myprintf.so -fPIC test.c
+export LD_PRELOAD=myprintf.so:$LD_LIBRARY_PATH
